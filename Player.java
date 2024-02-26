6e17e6f966310842d9929f98d7e0fb4b08db1404
matthewtranmer <matthewtranmer@outlook.com>
2024-02-26 16:17:22 +0000

    removed constructor from player class

--- Player.java
+++ Player.java
@@ -4,15 +4,6 @@ public class Player {
 
     private Rectangle[] rectangles;
 
-    public Player(int x, int y){
-        this.x = x;
-        this.y = y;
-
-        rectangles = new Rectangle[]{
-            new Rectangle(x, y, 50, 50, "green")
-        };
-    }
-
     public void addTo(GameArena gameArena){
         for (Rectangle rectangle : rectangles) {
             gameArena.addRectangle(rectangle);
